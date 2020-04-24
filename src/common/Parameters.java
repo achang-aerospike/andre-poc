435d013d3975d3a10f8411e11a044980a34cb2d1
Andre Chang <achang@aerospike.com>
2020-04-24 13:58:11 -0700

    Create package "common"

---
+++ src/common/Parameters.java
@@ -0,0 +1,23 @@
+package common;
+public class Parameters {
+	String host;
+	int port;
+	String user;
+	String password;
+	/*
+	String namespace;
+	String set;
+	AuthMode authMode;
+	TlsPolicy tlsPolicy;
+	WritePolicy writePolicy;
+	Policy policy;
+	EventLoopType eventLoopType = EventLoopType.DIRECT_NIO;
+	int maxCommandsInProcess;
+	int maxCommandsInQueue;
+	boolean singleBin;
+	boolean hasGeo;
+	boolean hasUdf;
+	boolean hasCDTList;
+	boolean hasCDTMap;
+	*/
+}
